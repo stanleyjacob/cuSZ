7eb642ebaa6ed368f8a3842b84ff0552b6d6e695
J. Tian <tian.jn09@gmail.com>
2020-09-23 22:53:52 -0500

    add lorenzo trait

---
+++ src/lorenzo_trait.cuh
@@ -0,0 +1,34 @@
+/**
+ * @file lorenzo_trait.cuh
+ * @author Jiannan Tian
+ * @brief
+ * @version 0.1.1
+ * @date 2020-09-23
+ *
+ * Copyright (c) 2020 by Washington State University, The University of Alabama, Argonne National Laboratory
+ *
+ */
+
+#include "cusz_dualquant.cuh"
+#include "metadata.hh"
+
+namespace zip {
+
+template <int ndim>
+struct Lorenzo_nd1l;
+
+}  // namespace zip
+
+namespace dryrun {
+
+template <int ndim>
+struct Lorenzo_nd1l;
+
+}  // namespace dryrun
+
+namespace unzip {
+
+template <int ndim>
+struct Lorenzo_nd1l;
+
+}  // namespace unzip
