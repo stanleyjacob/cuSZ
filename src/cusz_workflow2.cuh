a71aa147de2fe6124f428437d9fb9672d4aa79f0
J. Tian <tian.jn09@gmail.com>
2020-10-21 03:39:55 -0500

    change type trait

--- src/cusz_workflow2.cuh
+++ src/cusz_workflow2.cuh
@@ -1,3 +1,6 @@
+#ifndef CUSZ_WORKFLOW2_CUH
+#define CUSZ_WORKFLOW2_CUH
+
 /**
  * @file cusz_workflow2.cuh
  * @author Jiannan Tian
@@ -13,25 +16,31 @@
 #include <string>
 #include "argparse2_cusz.hh"
 #include "metadata.hh"
+#include "type_trait.hh"
 
 using std::string;
 
 namespace cusz {
 namespace interface {
 
-template <int ndim, int Block, typename Data, int QuantByte, int HuffByte>
-void Compress2(cuszContext* ctx, struct Metadata<Block>* m);
+template <int ndim, typename Data, int QuantByte, int HuffByte>
+void Compress2(cuszContext* ctx, typename MetadataTrait<ndim>::metadata_t* m);
 
-template <int ndim, int Block, typename Data, int QuantByte, int HuffByte>
-void Decompress2(cuszContext* ctx, struct Metadata<Block>* m);
+template <int ndim, typename Data, int QuantByte, int HuffByte>
+void Decompress2(cuszContext* ctx, typename MetadataTrait<ndim>::metadata_t* m);
 
 }  // namespace interface
 
 namespace impl {
 
-template <int ndim, int Block, typename Data, typename Quant>
-void VerifyHuffman(cuszContext* ctx, struct Metadata<Block>* m, Quant* xq);
+template <int ndim, typename Data, int QuantByte>
+void VerifyHuffman(
+    cuszContext*                              ctx,
+    typename MetadataTrait<ndim>::metadata_t* m,
+    typename QuantTrait<QuantByte>::Quant*    xq);
 
 }
 
 }  // namespace cusz
+
+#endif
