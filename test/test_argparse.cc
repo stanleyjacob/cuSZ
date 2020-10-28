ee28f6617f9b18a29a89ed140b6f46ab8b479dbe
J. Tian <tian.jn09@gmail.com>
2020-10-28 00:59:24 -0500

    change short doc

---
+++ test/test_argparse.cc
@@ -0,0 +1,19 @@
+/**
+ * @file test_argparse.cc
+ * @author Jiannan Tian
+ * @brief
+ * @version 0.1.3
+ * @date 2020-10-28
+ *
+ * (C) 2020 by Washington State University, Argonne National Laboratory
+ *
+ */
+
+#include "argparse2_cusz.hh"
+
+int main(int argc, char** argv)
+{
+    auto ctx = cuszContext(argc, argv);
+
+    return 0;
+}
